a8a6e1deb71c65c2843f91b1781b720f5ba327ea
kzk_maeda <casino.music0711@gmail.com>
2019-09-12 09:20:43 +0900

    message

--- demo-line-app/flask/main.py
+++ demo-line-app/flask/main.py
@@ -60,125 +60,62 @@ def handle_message(event):
   contents_string = """
   {
     "type": "bubble",
+    "header": {
+      "type": "box",
+      "layout": "vertical",
+      "contents": [
+        {
+          "type": "text",
+          "text": "あなたの性別を教えてください",
+          "color": "#ffffff",
+          "align": "start",
+          "size": "md",
+          "gravity": "center"
+        }
+      ],
+      "backgroundColor": "#27ACB2",
+      "paddingTop": "19px",
+      "paddingAll": "12px",
+      "paddingBottom": "16px"
+    },
     "body": {
       "type": "box",
       "layout": "vertical",
       "contents": [
         {
-          "type": "image",
-          "url": "https://line-objects-dev.com/flex/bg/eiji-k-1360395-unsplash.jpg",
-          "position": "relative",
-          "size": "full",
-          "aspectMode": "cover",
-          "aspectRatio": "1:1",
-          "gravity": "center"
-        },
-        {
-          "type": "box",
-          "layout": "horizontal",
-          "contents": [
-            {
-              "type": "box",
-              "layout": "vertical",
-              "contents": [
-                {
-                  "type": "text",
-                  "text": "Brown Hotel",
-                  "weight": "bold",
-                  "size": "xl",
-                  "color": "#ffffff"
-                },
-                {
-                  "type": "box",
-                  "layout": "baseline",
-                  "margin": "md",
-                  "contents": [
-                    {
-                      "type": "icon",
-                      "size": "sm",
-                      "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                    },
-                    {
-                      "type": "icon",
-                      "size": "sm",
-                      "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                    },
-                    {
-                      "type": "icon",
-                      "size": "sm",
-                      "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                    },
-                    {
-                      "type": "icon",
-                      "size": "sm",
-                      "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                    },
-                    {
-                      "type": "icon",
-                      "size": "sm",
-                      "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
-                    },
-                    {
-                      "type": "text",
-                      "text": "4.0",
-                      "size": "sm",
-                      "color": "#d6d6d6",
-                      "margin": "md",
-                      "flex": 0
-                    }
-                  ]
-                }
-              ]
-            },
-            {
-              "type": "box",
-              "layout": "vertical",
-              "contents": [
-                {
-                  "type": "text",
-                  "text": "¥62,000",
-                  "color": "#a9a9a9",
-                  "decoration": "line-through",
-                  "align": "end"
-                },
-                {
-                  "type": "text",
-                  "text": "¥42,000",
-                  "color": "#ebebeb",
-                  "size": "xl",
-                  "align": "end"
-                }
-              ]
-            }
-          ],
-          "position": "absolute",
-          "offsetBottom": "0px",
-          "offsetStart": "0px",
-          "offsetEnd": "0px",
-          "backgroundColor": "#00000099",
-          "paddingAll": "20px"
-        },
-        {
           "type": "box",
           "layout": "vertical",
+          "spacing": "md",
           "contents": [
             {
-              "type": "text",
-              "text": "SALE",
-              "color": "#ffffff"
+              "type": "button",
+              "style": "primary",
+              "action": {
+                "type": "uri",
+                "label": "Man",
+                "uri": "https://example.com"
+              }
+            },
+            {
+              "type": "button",
+              "style": "primary",
+              "action": {
+                "type": "uri",
+                "label": "Woman",
+                "uri": "https://example.com"
+              }
             }
           ],
-          "position": "absolute",
-          "backgroundColor": "#ff2600",
-          "cornerRadius": "20px",
-          "paddingAll": "5px",
-          "offsetTop": "10px",
-          "offsetEnd": "10px",
-          "paddingStart": "10px",
-          "paddingEnd": "10px"
+          "flex": 1
         }
       ],
-      "paddingAll": "0px"
+      "spacing": "md",
+      "paddingAll": "12px"
+    },
+    "styles": {
+      "footer": {
+        "separator": false
+      }
     }
   }
   """
